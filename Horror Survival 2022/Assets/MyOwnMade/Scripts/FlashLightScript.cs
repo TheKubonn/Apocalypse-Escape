b461475f4f1f60fde65aea4f0328b67a2077a801
F1ZZ <TheKubonn@hotmail.com>
2024-08-07 22:26:14 +0200

    flashlight battery power drain (change UI of battery chunks)

---
+++ Horror Survival 2022/Assets/MyOwnMade/Scripts/FlashLightScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashLightScript : MonoBehaviour
+{
+    private Image batteryChunks;
+    public float batteryPower = 1.0f;
+    public float drainTime = 2.0f;
+    
+    void Start()
+    {
+        batteryChunks = GameObject.Find("FlashLightBatteryChunks").GetComponent<Image>();
+        InvokeRepeating("FLBatteryDrain", drainTime, drainTime);
+    }
+    
+    void Update()
+    {
+        batteryChunks.fillAmount = batteryPower;
+    }
+
+    private void FLBatteryDrain()
+    {
+        if (batteryPower > 0.0f)
+        {
+            batteryPower -= 0.25f;
+        }
+    }
+}

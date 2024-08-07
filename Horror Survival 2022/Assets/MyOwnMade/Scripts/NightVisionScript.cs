65ebf76681a3e23752cd1b7e8f436f1ec5741e5f
F1ZZ <TheKubonn@hotmail.com>
2024-08-07 21:30:41 +0200

    Made a night vision UI and zooming im

---
+++ Horror Survival 2022/Assets/MyOwnMade/Scripts/NightVisionScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NightVisionScript : MonoBehaviour
+{
+    private Image zoomBar;
+    private Camera cam;
+    
+    void Start()
+    {
+        zoomBar = GameObject.Find("ZoomBar").GetComponent<Image>();
+        cam = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>();
+    }
+    
+    void Update()
+    {
+        ZoomIn();
+    }
+
+    private void ZoomIn()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (cam.fieldOfView > 10)
+            {
+                cam.fieldOfView -= 5;
+                zoomBar.fillAmount = cam.fieldOfView / 100;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (cam.fieldOfView < 60)
+            {
+                cam.fieldOfView += 5;
+                zoomBar.fillAmount = cam.fieldOfView / 100;
+            }
+        }
+    }
+}

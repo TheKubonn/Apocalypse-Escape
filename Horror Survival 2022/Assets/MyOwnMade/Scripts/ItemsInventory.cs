1ae50d1f0dda6591d4428d3d5d39a067318352aa
F1ZZ <TheKubonn@hotmail.com>
2024-08-30 21:10:13 +0200

    Item inventory script - literally the same as weapon inventory

--- Horror Survival 2022/Assets/MyOwnMade/Scripts/ItemsInventory.cs
+++ Horror Survival 2022/Assets/MyOwnMade/Scripts/ItemsInventory.cs
@@ -1,16 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemsInventory : MonoBehaviour
 {
+    public Sprite[] bigIcons;
+    public Image bigIcon;
+    public string[] titles;
+    public Text title;
+    public string[] descriptions;
+    public Text description;
+    public Button[] itemButtons;
+
+    private int chosenItemNumber;
+    private AudioSource audioPlayer;
+    public AudioClip click, select;
+    
     void Start()
     {
+        audioPlayer = GetComponent<AudioSource>();
         
+        bigIcon.sprite = bigIcons[0];
+        title.text = titles[0];
+        description.text = descriptions[0];
     }
     
     void Update()
     {
         
+    }
+    
+    public void ChooseItem(int itemNumber)
+    {
+        bigIcon.sprite = bigIcons[itemNumber];
+        title.text = titles[itemNumber];
+        description.text = descriptions[itemNumber];
+        audioPlayer.clip = click;
+        audioPlayer.Play();
+        chosenItemNumber = itemNumber;
     }
 }
